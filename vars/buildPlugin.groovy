ea17f2908d0d0d38b786267757eed5d912206ae9
monikakasture <kasturemonika14@gmail.com>
2023-02-05 16:50:36 -0500

    edit

--- vars/buildPlugin.groovy
+++ vars/buildPlugin.groovy
@@ -1,6 +1,6 @@
 def call(Map config) {
     node {
-        git url: "https://github.com/jenkinsci/${config.name}-plugin.git"
+        git url: "https://github.com/kasturenishant/Jenkins-Terraform.git" 
         sh 'mkdir test'
     }
 }
